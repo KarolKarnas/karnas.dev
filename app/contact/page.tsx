c306915b9031b7c75b18791ec9856b0bbcdc68e8
KarolKarnas <karol.karnas@gmail.com>
2023-12-19 13:04:48 +0100

    feat: Next.js project setup
    - create project with 'npx create-next-app@latest' (KD-10)
    - remove boilerplate code
    - install sass 'npm install --save-dev sass'
    - create basic design system (KD-8)

---
+++ app/contact/page.tsx
@@ -0,0 +1,10 @@
+import styles from './page.module.scss';
+
+export default function Contact() {
+	return (
+		<div>
+			<div className={styles.title}>Contact</div>
+
+		</div>
+	);
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import styles from './page.module.scss'
+
+export default function Home() {
+  return (
+    <main >
+     <h1 className={styles.title}>hello</h1>
+    </main>
+  )
+}
