70bbc58555ebf8158c6bc71c5209a16fe68e8ea5
KarolKarnas <karol.karnas@gmail.com>
2024-02-05 07:07:44 +0100

    refactor: Introduce SkillSection molecule component
    - Enhance code readability by creating a SkillSection molecule component,
    - Define an array of skills and utilize the map function to render SkillSection components, implement in Skill Page

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -1,24 +1,33 @@
 import styles from "./page.module.scss"
-import { PROGRAMMING, BACKEND, FRONTEND, TEST,DEV_OPS } from "../utils/constants"
+import {
+  PROGRAMMING,
+  BACKEND,
+  FRONTEND,
+  TEST,
+  DEV_OPS,
+} from "../utils/constants"
 import useIcon from "../hooks/useIcon"
 import StackIcons from "../ui/atoms/stackIcons"
 import MainHeading from "../ui/atoms/mainHeading"
 import MainSubHeading from "../ui/atoms/mainSubHeading"
 import SectionHeading from "../ui/atoms/sectionHeading"
+import SkillSection from "../ui/molecules/skillSection"
 import { Metadata } from "next"
 
 export const metadata: Metadata = {
-  title: 'Skills',
-  description: 'All my hard earn knowledge and skills on the one page :). TypeScript, JavaScript, React, Node.js, Next.js, PostgreSQL, Linux, Testing Library, Playwright, Tailwind, SASS'
-};
+  title: "Skills",
+  description:
+    "All my hard earn knowledge and skills on the one page :). TypeScript, JavaScript, React, Node.js, Next.js, PostgreSQL, Linux, Testing Library, Playwright, Tailwind, SASS",
+}
 
 export default async function About() {
-  const frontendIcons = useIcon(FRONTEND)
-  const backendIcons = useIcon(BACKEND)
-  const programmingIcons = useIcon(PROGRAMMING)
-  const testIcons = useIcon(TEST)
-  const devOpsIcons = useIcon(DEV_OPS)
-  // const session = await auth()
+  const skills = [
+    { title: "Programming", icons: useIcon(PROGRAMMING) },
+    { title: "Front-end", icons: useIcon(FRONTEND) },
+    { title: "Back-end", icons: useIcon(BACKEND) },
+    { title: "Test", icons: useIcon(TEST) },
+    { title: "DevOps", icons: useIcon(DEV_OPS) },
+  ]
 
   return (
     <div className={styles.container}>
@@ -27,26 +36,12 @@ export default async function About() {
         All my hard earn knowledge and skills on the one page :)
       </MainSubHeading>
 
-      <div className={styles.SkillSection}>
-        <SectionHeading color="purple">Programming</SectionHeading>
-        <StackIcons size="large" icons={programmingIcons} texts />
-      </div>
-      <div className={styles.SkillSection}>
-        <SectionHeading color="purple">Frontend</SectionHeading>
-        <StackIcons size="large" icons={frontendIcons} texts />
-      </div>
-      <div className={styles.SkillSection}>
-        <SectionHeading color="purple">Backend</SectionHeading>
-        <StackIcons size="large" icons={backendIcons} texts />
-      </div>
-      <div className={styles.SkillSection}>
-        <SectionHeading color="purple">Test</SectionHeading>
-        <StackIcons size="large" icons={testIcons} texts />
-      </div>
-      <div className={styles.SkillSection}>
-        <SectionHeading color="purple">DevOps</SectionHeading>
-        <StackIcons size="large" icons={devOpsIcons} texts />
-      </div>
+      {skills.map((skill, index) => (
+        <SkillSection key={index}>
+          <SectionHeading color="purple">{skill.title}</SectionHeading>
+          <StackIcons size="large" icons={skill.icons} texts />
+        </SkillSection>
+      ))}
     </div>
   )
 }
